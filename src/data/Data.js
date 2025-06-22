b7f83436d03ced19fcc58f8cb6a549cf7112ca31
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-22 20:14:49 +0300

    create an array of objects that holds data from a list buttons

---
+++ src/data/Data.js
@@ -0,0 +1,23 @@
+
+export const Data = [
+  {
+    "id": 1,
+    "name": "Laptop",
+  },
+  {
+    "id": 2,
+    "name": "Mobile",
+  },
+  {
+    "id": 3,
+    "name": "Tablet",
+  },
+  {
+    "id": 4,
+    "name": "Camera",
+  },
+  {
+    "id": 5,
+    "name": "Watch",
+  }
+]
