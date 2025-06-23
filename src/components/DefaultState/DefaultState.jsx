e0cf26d2064faec1ea4d0ed423cf088d231c86b4
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-23 20:09:05 +0300

    fix the UI of 2 component buttons

--- src/components/DefaultState/DefaultState.jsx
+++ src/components/DefaultState/DefaultState.jsx
@@ -2,7 +2,10 @@
 export default function DefaultState({ className, children, setShowDefault, showDefault, defaultValue }) {
   return (
     <div className={className}>
-      <h2 onClick={() => setShowDefault(!showDefault)}>{defaultValue}</h2>
+      <div className='mb-3 text-2xl border-2 border-neutral-600 py-2 px-3 rounded-xl flex justify-between cursor-pointer hover:border-neutral-400 transition-all duration-150' onClick={() => setShowDefault(!showDefault)}>
+        <div className='select-none'>{defaultValue}</div>
+        <div className={`${showDefault ? 'rotate-180' : 'rotate-0'} text-lg pt-1 select-none`}>V</div>
+      </div>
       {children}
     </div>
   );
--- src/components/SelectedState/SelectedState.jsx
+++ src/components/SelectedState/SelectedState.jsx
@@ -2,7 +2,10 @@
 export default function SelectedState({ className, children, setShowSelected, showSelected, selected }) {
   return (
     <div className={className}>
-      <h2 onClick={() => setShowSelected(!showSelected)}>{selected}</h2>
+      <div className='mb-3 text-2xl border-2 border-neutral-600 py-2 px-3 rounded-xl flex justify-between cursor-pointer hover:border-neutral-400 transition-all duration-150' onClick={() => setShowSelected(!showSelected)}>
+        <div className='select-none'>{selected}</div>
+        <div className={`${showSelected ? 'rotate-180' : 'rotate-0'} text-lg pt-1 select-none`}>V</div>
+      </div>
       {children}
     </div>
   )
