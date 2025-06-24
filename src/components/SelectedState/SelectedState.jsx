c53e0db18583dcbe40e3552c1e48e7ce3eb7b791
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-06-24 19:23:44 +0300

    make the texts responsive

--- src/components/SelectedState/SelectedState.jsx
+++ src/components/SelectedState/SelectedState.jsx
@@ -3,8 +3,8 @@ export default function SelectedState({ className, children, setShowSelected, sh
   return (
     <div className={className}>
       <div className='mb-3 text-2xl border-2 border-neutral-600 py-2 px-3 rounded-xl flex justify-between cursor-pointer hover:border-neutral-400 transition-all duration-150' onClick={() => setShowSelected(!showSelected)}>
-        <div className='select-none'>{selected}</div>
-        <div className={`${showSelected ? 'rotate-180' : 'rotate-0'} text-lg pt-1 select-none`}>V</div>
+        <div className='select-none text-[clamp(0.7rem,1.3rem,2rem)] pr-2'>{selected}</div>
+        <div className={`${showSelected ? 'rotate-180' : 'rotate-0'} text-lg pt-1 select-none text-[clamp(1rem,1rem,1.5rem)]`}>V</div>
       </div>
       {children}
     </div>
